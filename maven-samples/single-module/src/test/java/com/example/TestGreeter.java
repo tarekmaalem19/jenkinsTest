4d2fea052d458ed38d8324978e9acca566853fc1
tarek maalem <tarekmaale19@gmail.com>
2023-01-15 21:43:37 +0100

    first commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,6 +34,13 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  @Test
+  public void greetShouldIncludeGreetingPhraseWorld() {
+    String someone = "World hello again";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
